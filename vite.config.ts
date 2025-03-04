07b22d7725c9358c0e7d7948fda91687547b643d
Miha Filej <miha@filej.net>
2025-03-04 19:23:48 +0100

    Manual cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import type { Plugin } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss()] as Plugin[],
 })
